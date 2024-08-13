6964f292acb393d7d6f5222c7e05d703b0e0b440
Duyen <daphne.duyenhuynh@gmail.com>
2024-08-13 11:50:29 +0700

    Bt 4_Tinh S(n) = 1/2 + 1/4 + … + 1/2n, voi n duoc nhap tu ban phim (dung ham 'while'). Khai bao so nguyen 'int' va so thuc 'float', chu y ep kieu va ket qua so thap phan

---
+++ 04.cpp
@@ -0,0 +1,17 @@
+// Bài 4: Tính S(n) = ½ + ¼ + … + 1/2n
+
+#include <stdio.h>
+int main()
+{
+    int i = 1,  n;
+    float s = 0;
+    printf("n = ");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        s += (float)1/(2*i);
+        i ++;
+    }
+    printf("S(n) = %.5f\n", s);
+    return 0;
+}
