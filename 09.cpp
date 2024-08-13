a5548383aefa5d18cd05033e0a82a03f8900286f
Duyen <daphne.duyenhuynh@gmail.com>
2024-08-13 14:55:04 +0700

    Bài 9 tuong tu cac bai truoc. Chu y logic

---
+++ 09.cpp
@@ -0,0 +1,17 @@
+// Bài 9: Tính T(n) = 1 x 2 x 3…x N, n nguyen duong
+
+#include <stdio.h>
+int main()
+{
+    int i = 1, s = 1, n;
+
+    printf("n = ");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        s *= n;
+        i ++;
+    }
+    printf("S(n) = %d\n", s);
+    return 0;
+}
