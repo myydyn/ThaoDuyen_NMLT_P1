611b7445036f852a8fbe9e4350273f964e883971
Duyen <daphne.duyenhuynh@gmail.com>
2024-08-13 14:32:11 +0700

    Bài 7: Tính S(n) = n/(n+1) tuong tu cac bai truoc. Chu y logic

---
+++ 07.cpp
@@ -0,0 +1,17 @@
+// Bài 7: Tính S(n) = ½ + 2/3 + ¾ + …. + n / n + 1
+
+#include <stdio.h>
+int main()
+{
+    int i = 1,  n;
+    float s = 0;
+    printf("n = ");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        s += (float)i/(i+1);
+        i ++;
+    }
+    printf("S(n) = %.5f\n", s);
+    return 0;
+}
