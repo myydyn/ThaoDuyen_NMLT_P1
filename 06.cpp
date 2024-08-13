5cb1452dce95e72b9f8b5278e7622b72b04718ec
Duyen <daphne.duyenhuynh@gmail.com>
2024-08-13 12:14:42 +0700

    Bài 6: Tính S(n) = 1/1×2 + 1/2×3 +…+ 1/n x (n + 1), tuong tu cac bai truoc. Chu y logic

--- 06.cpp
+++ 06.cpp
@@ -1,15 +1,15 @@
-// Bài 5: Tính S(n) = 1 + 1/3 + 1/5 + … + 1/(2n + 1), n nguyen duong
+// Bài 6: Tính S(n) = 1/1×2 + 1/2×3 +…+ 1/n x (n + 1), n nguyen duong
 
 #include <stdio.h>
 int main()
 {
-    int i = 0,  n; // i=1 thi cho s=1
+    int i = 1,  n;
     float s = 0;
     printf("n = ");
     scanf("%d", &n);
     while (i <= n)
     {
-        s += (float)1/(2*i+1);
+        s += (float)1/(i*(i+1));
         i ++;
     }
     printf("S(n) = %.5f\n", s);
